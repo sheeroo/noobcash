de111f3c080b254f14a4871762881a33ad936f14
adonistseriotis <adonis.tseriotis@gmail.com>
2022-03-26 19:43:58 +0200

    - Block class - Init blockchain class

--- block.py
+++ block.py
@@ -1,22 +1,21 @@
 
-import blockchain
-
-
-
+# import blockchain
+from time import time
+import hashlib
 
 class Block:
-	def __init__(self):
-		##set
+    def __init__(self, index, previousHash, nonce, listOfTransactions, timestamp):
+        ##set self.previousHash self.timestamp self.hash self.nonce self.listOfTransactions
+        self.index = index
+        self.timestamp = timestamp or time.time()
+        self.transactions = listOfTransactions
+        self.nonce = nonce
+        self.previous_hash = previousHash
+        self.current_hash = self.myHash()
 
-		#self.previousHash
-		#self.timestamp
-		#self.hash
-		#self.nonce
-		#self.listOfTransactions
-	
-	def myHash:
-		#calculate self.hash
+    def myHash(self):
+        block_of_string = "{}{}{}{}{}".format(self.previousHash, self.nonce, self.listOfTransactions, self.timestamp)
+        self.hash = hashlib.sha256(block_of_string.encode()).hexdigest()
 
-
-	def add_transaction(transaction transaction, blockchain blockchain):
-		#add a transaction to the block
+    # def add_transaction(Transaction transaction, Blockchain blockchain):
+        #add a transaction to the block

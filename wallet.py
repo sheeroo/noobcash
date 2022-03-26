6a9e2f0389b565ca19ace2a0d87f25ba2af571f7
Thodorissio <thodorissiozos@gmail.com>
2022-03-26 18:35:55 +0200

    wallet 1st implementation

--- wallet.py
+++ wallet.py
@@ -1,28 +1,14 @@
 import binascii
 
-import Crypto
-import Crypto.Random
-from Crypto.Hash import SHA
+#import Crypto
+from Crypto import Random
+#from Crypto.Hash import SHA
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 
-import hashlib
-import json
-from time import time
-from urllib.parse import urlparse
-from uuid import uuid4
-
-
-
 class wallet:
 
-	def __init__():
-		##set
-
-		#self.public_key
-		#self.private_key
-		#self_address
-		#self.transactions
-
-	def balance():
-
+	def __init__(self):
+		key = RSA.generate(2048)
+		self.public_key = key.publickey().export_key(format = 'PEM')
+		self.private_key = key.export_key(format = 'PEM')
